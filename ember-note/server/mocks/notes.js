b3cf49c8d6131b19c3f090216ed0e6fbf0cc9892
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-09-30 21:22:39 -0700

    Apply pattern of JSON-API compliance to mock notes

--- ember-note/server/mocks/notes.js
+++ ember-note/server/mocks/notes.js
@@ -1,4 +1,10 @@
 /*jshint node:true*/
+
+/*
+  NOTE: http://www.per-aspera.eu/tutorial-ember-2-http-mock-for-json-api-adapter
+  was very helpful
+*/
+
 module.exports = function(app) {
   var express = require('express');
   var notesRouter = express.Router();
@@ -16,22 +22,24 @@ module.exports = function(app) {
   });
 
   notesRouter.post('/', function(req, res) {
-    noteDB.find({}).sort({id : -1}).limit(1).exec(function(err, notes) {
-      if(notes.length != 0)
-        req.body.note.id =  notes[0].id + 1;
-      else
-        req.body.note.id = 1;
-      noteDB.insert(req.body.note, function(err, newNote) {
-        res.status(201);
-        res.send(
-          JSON.stringify(
-          {
-            note : newNote
-          }));
-      });
-    })
+    var data = req.body.data;
+    var note = data.attributes;
+    noteDB.find({}).sort({id : -1}).limit(1).exec(
+      function(err, notes) {
+        if(notes.length != 0)
+          note.id =  notes[0].id + 1;
+        else
+          note.id = 1;
+        noteDB.insert(note, function(err, newNote) {
+          res.status(201);
+          res.send(
+            JSON.stringify(
+            {
+              note : newNote
+            }));
+        });
+      })
   });
-
 
   notesRouter.get('/:id', function(req, res) {
     res.send({
@@ -44,22 +52,21 @@ module.exports = function(app) {
   notesRouter.put('/:id', function(req, res) {
     var id = parseInt(req.params.id);
     noteDB.update({id: id}, {$set: req.body.note},
-      function(err, numReplaced, newNotes) {
-        res.send({
-          'notes': {
-            id: id
-          }
-        });
-      }
-    );
-  });
-
-  notesRouter.delete('/:id', function(req, res) {
-    var id = parseInt(req.params.id);
-    noteDB.remove({id: id}, function(err, numRemoved) {
-      res.status(204).end();
+    function(err, numReplaced, newNotes) {
+      res.send({
+        'notes': {
+          id: id
+        }
+      });
     });
   });
 
-  app.use('/api/notes', notesRouter);
+  notesRouter.delete('/:id', function(req, res) {
+		var id = parseInt(req.params.id);
+		noteDB.remove({id: id}, function(err, numRemoved) {
+			res.status(204).end();
+		});
+  });
+
+  app.use('/api/notes', require('body-parser').json({ type: 'application/vnd.api+json' }), notesRouter);
 };
