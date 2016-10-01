681d7e033dcecbe99bef7fc27480dcca4f5c359f
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-09-30 21:09:39 -0700

    ember g http-mock notes

---
+++ ember-note/server/mocks/notes.js
@@ -0,0 +1,47 @@
+/*jshint node:true*/
+module.exports = function(app) {
+  var express = require('express');
+  var notesRouter = express.Router();
+
+  notesRouter.get('/', function(req, res) {
+    res.send({
+      'notes': []
+    });
+  });
+
+  notesRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  notesRouter.get('/:id', function(req, res) {
+    res.send({
+      'notes': {
+        id: req.params.id
+      }
+    });
+  });
+
+  notesRouter.put('/:id', function(req, res) {
+    res.send({
+      'notes': {
+        id: req.params.id
+      }
+    });
+  });
+
+  notesRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  // The POST and PUT call will not contain a request body
+  // because the body-parser is not included by default.
+  // To use req.body, run:
+
+  //    npm install --save-dev body-parser
+
+  // After installing, you need to `use` the body-parser for
+  // this mock uncommenting the following line:
+  //
+  //app.use('/api/notes', require('body-parser').json());
+  app.use('/api/notes', notesRouter);
+};
