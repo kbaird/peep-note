2d5816cd9965749a930a69e42350346df1f4db86
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-10-03 13:39:17 -0700

    Hack to re-set user name in login route

--- ember-note/app/login/route.js
+++ ember-note/app/login/route.js
@@ -8,6 +8,9 @@ export default Ember.Route.extend({
       }).then((users) => {
         if(users.get('length') === 1) {
           var user = users.objectAt(0);
+          user.name = this.controller.get('name');
+          console.log("name = " + JSON.stringify(this.controller.get('name')));
+          console.log("user = " + JSON.stringify(user));
           this.controllerFor('application').set('user', user);
           this.transitionTo('notebooks');
           this.transitionTo('notebooks', user.get('id'));
