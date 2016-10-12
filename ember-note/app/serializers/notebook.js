74c30d2de3dd6127fa5bf0e77e355772b737f9da
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-10-12 14:25:34 -0700

    Add normalize & serialize to notebook serializer as per PDF pg83

--- ember-note/app/serializers/notebook.js
+++ ember-note/app/serializers/notebook.js
@@ -1,4 +1,18 @@
-import ApplicationSerializer from './application';
+import DS from 'ember-data';
 
-export default ApplicationSerializer.extend({
+export default DS.RESTSerializer.extend({
+  normalize: function(typeClass, hash, prop) {
+    hash.title = hash.Title;
+    delete hash.Title;
+    hash.user = hash.User;
+    delete hash.User;
+    return this._super(typeClass, hash, prop);
+  },
+  serialize: function(snapshot, options) {
+    var json = {
+      Title: snapshot.attr('title'),
+      User:  snapshot.belongsTo('user').id
+    };
+    return json;
+  },
 });
