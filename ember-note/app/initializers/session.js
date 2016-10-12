03bb7cddf326d6f8ba67fddd22738fa6f0ef98c0
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-10-12 14:02:12 -0700

    Modify session initializer as per PDF pg79

--- ember-note/app/initializers/session.js
+++ ember-note/app/initializers/session.js
@@ -1,8 +1,13 @@
-export function initialize(/* application */) {
-  // application.inject('route', 'foo', 'service:foo');
+import Ember from 'ember';
+
+export function initialize(container, application) {
+   var session = Ember.Object.extend();
+   application.register('session:main', session);
+   application.inject('adapter', 'session', 'session:main');
+   application.inject('route',   'session', 'session:main');
 }
 
 export default {
-  name: 'session',
-  initialize
+  name:       'session',
+  initialize: initialize
 };
